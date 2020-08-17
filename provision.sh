3691fcb78762ecf8077e48d872d0f1e336964b22
MOH <aallai@spartaglobal.com>
2020-08-17 15:07:23 +0100

    added provision.sh

--- provision.sh
+++ provision.sh
@@ -1,13 +1,13 @@
-!#/bin/bash
+#!/bin/bash
 
 # this provision script should be run on the controller vm (aws in our case)
 
-sudo apt update
+sudo apt update -y
 
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 
-sudo apt-get install tree
+sudo apt-get install tree -y
 
-sudo apt install ansible
+sudo apt install ansible -y 
